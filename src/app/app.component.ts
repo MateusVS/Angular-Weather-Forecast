bdd738c2e6b118d1c06aaf0283df5a922ae7c860
Mateus Vinicius da Silva <36384659+MateusVS@users.noreply.github.com>
2025-03-20 18:34:00 +0000

    Inicio app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    FormsModule, 
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
