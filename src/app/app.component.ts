1728444dda7f7be8b31cc03fd9ccf9b5ba7a9b63
Mateus Vinicius da Silva <36384659+MateusVS@users.noreply.github.com>
2025-03-27 02:32:22 +0000

    Add search form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
-    FormsModule, 
-    FontAwesomeModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { WheaterService } from '../../services/wheater.service';
 import { WeatherDatas } from '../../../../models/interfaces/weather-datas.interface';
 
 @Component({
   selector: 'app-wheater-home',
-  imports: [],
+  imports: [
+    FormsModule, 
+    FontAwesomeModule,
+  ],
   templateUrl: './wheater-home.component.html',
 })
-export class WheaterHomeComponent implements OnInit {
+export class WheaterHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'São Paulo';
   weatherDatas! :WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WheaterService) {}
 
@@ -19,11 +28,24 @@ export class WheaterHomeComponent implements OnInit {
 
   getWeatherDatas(cityName: string): void {
     this.weatherService.getWeatherDatas(cityName)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (response) => {
           response && (this.weatherDatas = response);
         },
         error: (error) => console.log(error),
-      })
+      });
+  }
+
+  OnSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
